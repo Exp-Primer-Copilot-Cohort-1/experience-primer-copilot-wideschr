cec61b2f4572a4bbe5767b711a6aced2276fe7be
wideschr <147988280+wideschr@users.noreply.github.com>
2023-10-18 15:20:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  const skills = document.querySelectorAll('.skill');
+  const skillsArray = Array.from(skills);
+  skillsArray.forEach((skill) => {
+    skill.addEventListener('click', (event) => {
+      event.currentTarget.classList.toggle('skill-selected');
+    });
+  });
+}
+
